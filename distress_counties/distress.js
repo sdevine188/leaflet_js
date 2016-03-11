19e890f72df94be908d14c3380bce124ff7b6e6b
sdevine188 <sdevine188@gmail.com>
2016-03-10 22:46:48 -0500

    working version of county map, need to merge unemp and pc_inc data, general clean up

---
+++ distress_counties/distress.js
@@ -0,0 +1,128 @@
+// var states = states_json2()
+// states[0].features[0].properties.density
+var counties = counties_json()
+
+
+$(document).ready(function(){
+
+	// var states = states_json2()
+	console.log(counties)
+
+	var map = L.map('map').setView([37.8, -96], 4)
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{
+		attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+		// maxZoom: 17,
+		// minZoom: 9
+		}).addTo(map)
+	L.geoJson(counties).addTo(map);
+
+	// make choropleth of state overlays
+	function getColor(d) {
+	    	return d > 1000 ? '#800026' :
+	           d > 500  ? '#BD0026' :
+	           d > 200  ? '#E31A1C' :
+	           d > 100  ? '#FC4E2A' :
+	           d > 50   ? '#FD8D3C' :
+	           d > 20   ? '#FEB24C' :
+	           d > 10   ? '#FED976' :
+	                      '#FFEDA0';
+	}
+
+	function style(feature) {
+		return {
+			fillColor: getColor(Number(feature.properties.STATEFP)),
+			weight: 2,
+			opacity: 1,
+			color: 'white',
+			dashArray: '3',
+			fillOpacity: 0.7
+		};
+	}
+
+	L.geoJson(counties, {style: style}).addTo(map)
+
+	// highlight on mouseover
+	var geojson
+
+	function highlightFeature(e) {
+		var layer = e.target;
+
+		layer.setStyle({
+			weight: 5,
+			color: '#666',
+			dashArray: '',
+			fillOpacity: 0.7
+		});
+
+		if (!L.Browser.ie && !L.Browser.opera) {
+			layer.bringToFront();
+		}
+
+		info.update(layer.feature.properties)
+	}
+
+	function resetHighlight(e) {
+		geojson.resetStyle(e.target)
+		info.update()
+	}
+
+	function zoomToFeature(e) {
+    		map.fitBounds(e.target.getBounds());
+	}
+
+	function onEachFeature(feature, layer) {
+		layer.on({
+			mouseover: highlightFeature,
+			mouseout: resetHighlight,
+			click: zoomToFeature
+		})
+	}
+
+	geojson = L.geoJson(counties, {
+		style: style,
+		onEachFeature: onEachFeature
+	}).addTo(map)
+
+	// create custom control box when hovering over state
+	var info = L.control();
+
+	info.onAdd = function (map) {
+	    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+	    this.update();
+	    return this._div;
+	};
+
+	// method that we will use to update the control based on feature properties passed
+	info.update = function (props) {
+	    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+	        '<b>' + props.NAME + '</b><br />' + props.STATEFP + ' people / mi<sup>2</sup>'
+	        : 'Hover over a state');
+	};
+
+	info.addTo(map);
+
+	// create legend
+	var legend = L.control({position: 'bottomright'});
+
+	legend.onAdd = function (map) {
+
+	    var div = L.DomUtil.create('div', 'info legend'),
+	        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+	        labels = [];
+
+	    // loop through our density intervals and generate a label with a colored square for each interval
+	    for (var i = 0; i < grades.length; i++) {
+	        div.innerHTML +=
+	            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+	            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	        console.log(div.innerHTML)
+	    }
+
+	    return div;
+	};
+
+	legend.addTo(map);
+
+})
+
